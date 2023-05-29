23c0fcefbafea6adebdbed06222d94bb0ea8211e
Nyaka <dammnyaka@gmail.com>
2023-05-29 14:06:08 +0300

    add background color for html

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <div>
-    <StockComponent />
-  </div>
+   <div>
+      <StockComponent />
+   </div>
 </template>
 
 <script>
 import StockComponent from "./components/StockComponent.vue";
 
 export default {
-  name: "App",
-  components: {
-    StockComponent,
-  },
+   name: "App",
+   components: {
+      StockComponent,
+   },
 };
 </script>
 
 <style>
+html {
+   background-color: rgb(20, 20, 20);
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
+   margin-top: 60px;
 }
 </style>
